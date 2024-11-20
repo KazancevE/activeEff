437b84bfbe86084bb3b883b50a84523126c3bfb4
kazan <kazanceve.gor@gmail.com>
2024-11-20 17:44:54 +0700

    a

--- page/kth_project/index.js
+++ page/kth_project/index.js
@@ -15,7 +15,7 @@
         const dataGen = data['КТХ Проект' ]
          console.log(dataGen)
          dataGen.sort((a,b) => {
-            if (taskAtWork != "0"){
+            if (taskAtWork != '0') {
                 if (a.eff < b.eff) {
                     return -1; 
                 }
